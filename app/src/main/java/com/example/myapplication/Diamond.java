74684a47bf44a3ed392c4571687a62a4c1192b60
haiderrali5 <27100058@lums.edu.pk>
2026-02-16 22:42:45 +0500

    Add Diamond class that extends Shape

---
+++ app/src/main/java/com/example/myapplication/Diamond.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+public class Diamond extends Shape {
+    private int size;
+
+    public Diamond() {
+        this.size = 10;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
